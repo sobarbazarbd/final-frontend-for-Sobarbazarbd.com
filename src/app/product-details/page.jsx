498fb98182ba6128a14938fbaf13cd88c85c1057
Sobar Bazar BD <sobarbazarbd1@gmail.com>
2025-10-02 17:19:26 +0600

    Update page.jsx

--- src/app/product-details/page.jsx
+++ src/app/product-details/page.jsx
@@ -11,9 +11,9 @@ import Preloader from "@/helper/Preloader";
 import ScrollToTopInit from "@/helper/ScrollToTopInit";
 
 export const metadata = {
-  title: "MarketPro - E-commerce Next JS Template",
+  title: "SobazarBd - Oniline Shopping Complex",
   description:
-    "MarketPro is a comprehensive and versatile Next JS template designed for e-commerce platforms, specifically tailored for multi vendor marketplaces. With its modern design and extensive feature set, MarketPro provides everything you need to create a robust and user-friendly online marketplace..",
+    "hello",
 };
 
 const page = () => {
