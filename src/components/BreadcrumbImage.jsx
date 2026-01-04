4f9f163ddb0220a7cd0a36030429f688eae366f0
unknown <jhobelsaha@gmail.com>
2026-01-04 13:27:16 +0600

    added fields on became seller page

--- src/components/BreadcrumbImage.jsx
+++ src/components/BreadcrumbImage.jsx
@@ -21,7 +21,7 @@ const BreadcrumbImage = () => {
       <div className='container'>
         <div className='banner-inner__content text-center'>
           <h4 className='text-white mb-20 fw-semibold'>
-            Become A MarketPro Seller
+            Become a Seller – SobarbazarBD
           </h4>
           <p className='text-white my-20'>
             More than half the units sold in our stores from independent
